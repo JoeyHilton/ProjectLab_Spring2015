25f5ae622101eab8eee1cb081490b773f8a9b3aa
JoeyHilton <shnek81@gmail.com>
2015-03-25 16:12:26 -0600

    Added static pages, footer, and static pages test

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
-  root 'projects#index'
+  # get 'static_pages/home'
 
+  # get 'static_pages/about'
+
+  get 'static_pages/contact'
+
+  root 'static_pages#home'
+
+  get '/about', to: 'static_pages#about', as: :about
+
+  get '/contact', to: 'static_pages#contact', as: :contact
   # get 'projects/show'
 
   # get 'projects/new'
@@ -14,6 +23,7 @@ Rails.application.routes.draw do
   # get 'projects/destroy'
 
   resources :projects
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
