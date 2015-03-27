79dac551d9a9dd0ae61e1986536e48e8a119ada6
JoeyHilton <shnek81@gmail.com>
2015-03-27 17:35:44 -0600

    added tests to devise

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,15 @@ Rails.application.routes.draw do
 
   root 'static_pages#home'
 
+  devise_for :users, :controllers => { registrations: 'registrations' }
+
+  resources :users, :only => [:show, :index]
+  get 'profile', to: 'users#profile', as: :profile
+  
   get '/about', to: 'static_pages#about', as: :about
   get '/contact', to: 'static_pages#contact', as: :contact
 
   resources :projects 
 
-  devise_for :users, :controllers => { registrations: 'registrations' }
-  resources :users, :only => [:show, :index]
-  get 'users/profile'
   
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,17 +1,26 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
+
+  def setup
+    @user = users(:one)
+    @otheruser = users(:two)
+  end
+
   test "should get index" do
     get :index
     assert_response :success
   end
 
   test "should get show" do
-    get :show
+    sign_in @user
+    user = @otheruser
+    get :show, id: user.id
     assert_response :success
   end
 
   test "should get profile" do
+    sign_in @user
     get :profile
     assert_response :success
   end
