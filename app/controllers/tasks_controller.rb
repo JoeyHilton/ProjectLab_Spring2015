75fb9470e16fd84fd7243c59b2242666657b5407
JoeyHilton <shnek81@gmail.com>
2015-03-28 12:20:10 -0600

    Added fields to tasks and task table

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -45,7 +45,7 @@ class TasksController < ApplicationController
   end
 
   def task_params
-    params.require(:task).permit(:name, :description, :due_date, :project_id)
+    params.require(:task).permit(:name, :description, :due_date, :priority, :complete, :project_id)
   end
 end
     
